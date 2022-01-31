6b6c348813581b4c05a104bd042dd21fa5d73021
Elijahdanie <elijahdanie@gmail.com>
2022-01-31 15:43:14 +0100

    made more edits

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -9,20 +9,23 @@ import csv
 import requests
 import sys
 
+api_url = 'https://jsonplaceholder.typicode.com/'
+
 if __name__ == '__main__':
-    userid = int(sys.argv[1])
-    resp = requests.get(
-        'https://jsonplaceholder.typicode.com/users/{}'.format(userid))
-    name = resp.json()['name']
-    task_resp = requests.get(
-        'https://jsonplaceholder.typicode.com/todos/?userId={}'.format(userid))
-    task_json = task_resp.json()
-    with open('{}.csv'.format(userid), 'w', newline='') as csvfile:
-        writercsv = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        for i in task_json:
-            r = []
-            r.append(userid)
-            r.append(name)
-            r.append(str(i['completed']))
-            r.append(i['title'])
-            writercsv.writerow(r)
+    if len(sys.argv) > 0 and sys.argv[1].isnumeric():
+        userid = int(sys.argv[1])
+        resp = requests.get(
+            '{}users/{}'.format(api_url, userid))
+        name = resp.json()['name']
+        task_resp = requests.get(
+            '{}todos/?userId={}'.format(api_url, userid))
+        task_json = task_resp.json()
+        with open('{}.csv'.format(userid), 'w', newline='') as csvfile:
+            writercsv = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+            for i in task_json:
+                r = []
+                r.append(userid)
+                r.append(name)
+                r.append(str(i['completed']))
+                r.append(i['title'])
+                writercsv.writerow(r)
