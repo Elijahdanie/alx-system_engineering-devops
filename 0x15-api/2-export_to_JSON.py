46ce619c9f0599cac16815ca29244e4d5be1e369
Elijahdanie <elijahdanie@gmail.com>
2022-01-31 17:43:15 +0100

    ...

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -9,22 +9,25 @@ import json
 import requests
 import sys
 
+api_url = 'https://jsonplaceholder.typicode.com/'
+
 if __name__ == '__main__':
-    userid = int(sys.argv[1])
-    resp = requests.get(
-        'https://jsonplaceholder.typicode.com/users/{}'.format(userid))
-    name = resp.json()['name']
-    task_resp = requests.get(
-        'https://jsonplaceholder.typicode.com/todos/?userId={}'.format(userid))
-    c_task = 0
-    task_json = task_resp.json()
-    json_data = {userid: []}
-    for i in task_json:
-        r = {
-            'task': i['title'],
-            'completed': i['completed'],
-            'username': name,
-            }
-        json_data[userid].append(r)
-    with open('{}.json'.format(userid), 'w') as jsonfile:
-        json.dump(json_data, jsonfile)
+    if len(sys.argv) > 0 and sys.argv[1].isnumeric():
+        userid = int(sys.argv[1])
+        resp = requests.get(
+            '{}users/{}'.format(api_url, userid))
+        name = resp.json()['name']
+        task_resp = requests.get(
+            '{}todos/?userId={}'.format(api_url, userid))
+        c_task = 0
+        task_json = task_resp.json()
+        json_data = {userid: []}
+        for i in task_json:
+            r = {
+                'task': i['title'],
+                'completed': i['completed'],
+                'username': name,
+                }
+            json_data[userid].append(r)
+        with open('{}.json'.format(userid), 'w') as jsonfile:
+            json.dump(json_data, jsonfile)
