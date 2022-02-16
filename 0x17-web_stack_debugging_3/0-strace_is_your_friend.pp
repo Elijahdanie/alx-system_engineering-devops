20769a83ee9634c1c7cac659202d791d7cdcf1d7
Elijahdanie <elijahdanie@gmail.com>
2022-02-16 15:15:12 +0100

    fixed error

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+# Solve a 500 internal server error on apache
+exec { 'solve apache error':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php; sudo service apache2 restart',
+  path    => ['/usr/sbin']
+}
