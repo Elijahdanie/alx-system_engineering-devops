3397051333dd4f27f25e3080c44e4fba6327dc58
Elijahdanie <elijahdanie@gmail.com>
2022-02-16 05:56:25 +0100

    ...

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -19,11 +19,13 @@ def top_ten(subreddit):
         headers = {'User-Agent': 'elijahdanie'}
         resp = requests.get(link, headers=headers)
         data = resp.json()
+        print(resp.status_code)
         childparent = data.get('data')
         if childparent is not None and 'children' in childparent:
             children = childparent.get('children')
             for child in children:
                 print(child.get('data').get('title'))
+            return
         print('None')
     except BaseException:
         print('None')
