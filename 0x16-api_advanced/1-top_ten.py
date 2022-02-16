48bd7f490782de29928b5a12f57e19d77a9c88fc
Elijahdanie <elijahdanie@gmail.com>
2022-02-16 06:07:54 +0100

    fixed error

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -15,14 +15,19 @@ def top_ten(subreddit):
     10 hot post on the subreddit
     """
     try:
-        link = 'http://reddit.com/r/{}/hot/.json?count=10'.format(subreddit)
-        resp = requests.get(link)
+        link = 'http://reddit.com/r/{}/hot/.json?'.format(subreddit)
+        param = {'count': 10}
+        resp = requests.get(link, params=param)
         data = resp.json()
         childparent = data.get('data')
+        count = 0
         if childparent is not None and 'children' in childparent:
             children = childparent.get('children')
             for child in children:
+                count = count + 1
                 print(child.get('data').get('title'))
+                if count == 10:
+                    return
             return
         print('None')
     except BaseException:
