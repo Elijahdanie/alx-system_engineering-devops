998bb0a5d74edb902a0feb64fdbbdf324632f7a6
Elijahdanie <elijahdanie@gmail.com>
2022-02-17 11:02:24 +0100

    removed the sudo command

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Solve a 500 internal server error on apache
 exec { 'solve apache error':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php; sudo service apache2 restart',
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php; service apache2 restart',
   path    => ['usr/bin/', '/bin', '/usr/sbin']
 }
