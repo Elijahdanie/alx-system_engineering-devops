dd5dc173af5f57a3b098b0aa14939a0ace5bbcf3
Elijahdanie <elijahdanie@gmail.com>
2022-02-24 15:37:22 +0100

    reduced worker processes and removed sudo

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 # This confu=igures nginx server to handle more requests
 exec { 'fix failing requests':
-  command => "sed -i 's/worker_processes 4;/worker_processes 9;/g' /etc/nginx/nginx.conf; sudo service nginx restart",
+  command => "sed -i 's/worker_processes 4;/worker_processes 7;/g' /etc/nginx/nginx.conf; service nginx restart",
   path    => ['usr/bin/', '/bin', '/usr/sbin']
 }
