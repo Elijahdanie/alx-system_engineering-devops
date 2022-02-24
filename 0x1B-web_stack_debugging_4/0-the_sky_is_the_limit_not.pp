21844174b19b2d8b99198ea40b5c25dcaf898b1c
Elijahdanie <elijahdanie@gmail.com>
2022-02-24 15:25:44 +0100

    added root privileges to command

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,5 @@
--- This confu=igures nginx server to handle more
--- requests
+# This confu=igures nginx server to handle more requests
 exec { 'fix failing requests':
-  command => "sed -i 's/worker_processes 4;/worker_processes 9;/g' /etc/nginx/nginx.conf; service nginx restart",
+  command => "sed -i 's/worker_processes 4;/worker_processes 9;/g' /etc/nginx/nginx.conf; sudo service nginx restart",
   path    => ['usr/bin/', '/bin', '/usr/sbin']
 }
